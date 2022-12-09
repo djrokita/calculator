c9d5e5a8a91c4c5157e3a1512d19f0ab4b77edcb
Krzysztof Pierzchala <k.pierzchala@godeltech.com>
2022-12-09 13:28:23 +0100

    update webpack, remove jQuery

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
-const path = require('path');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/newScript.js',
+    entry: path.resolve(__dirname, "src/index.js"),
     output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js",
     },
-    watch: true,
     watchOptions: {
-        ignored: /node_modules/
-    }
-}
+        ignored: /node_modules/,
+    },
+    devtool: "source-map",
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, "public"),
+        },
+        port: 3000,
+        hot: true,
+        open: true,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: "index.html",
+            template: "index.html",
+        }),
+    ],
+    mode: "development",
+};
